486ea9d9ac8f0d387276e7f35dad74b85d7047df
APPLE <apple@APPLEs-MacBook-Air.local>
2025-01-10 23:51:24 +0530

    firstchanges

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
   content: [
     './pages/**/*.{js,jsx}',
@@ -79,5 +79,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  //plugins: [require("tailwindcss-animate")],
 }
